bcf8a9c5470b83f2d871e859c0daeef1adb0c840
StasyaV <46928311+StasyaV@users.noreply.github.com>
2020-07-29 17:33:23 +0300

    Удалила лишнюю функцию

--- src/main.js
+++ src/main.js
@@ -10,16 +10,6 @@ const renderElement = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const createHeaderInfoTemplate = () => {
-  return (
-    `<section class="trip-main__trip-info  trip-info">
-      <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">0</span>
-      </p>
-    </section>`
-  );
-};
-
 const createMenuTemplate = () => {
   return (
     `<nav class="trip-controls__trip-tabs  trip-tabs">
